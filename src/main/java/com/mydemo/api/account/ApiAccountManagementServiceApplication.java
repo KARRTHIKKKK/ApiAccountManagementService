bbc1eed8b764df83e9e89b1db68ecf66b4fc6d5e
KARRTHIKKKK <karthikeyapepeti05@gmail.com>
2026-04-07 12:18:42 +0530

    first commit

---
+++ src/main/java/com/mydemo/api/account/ApiAccountManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mydemo.api.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAccountManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAccountManagementServiceApplication.class, args);
+	}
+
+}
